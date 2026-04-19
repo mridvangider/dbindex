b31b32e453983b0258fa5974f0501fb8fe5b81ab
Muhammed Ridvan Gider <mridvang@gmail.com>
2026-04-19 11:54:05 +0300

    implemented the mcp server

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from dbindex!")
+from contextlib import asynccontextmanager
+from collections.abc import AsyncIterator
+import json
+import sys
+
+from mcp.server.fastmcp import FastMCP
+
+from app_context import AppContext, AppConfig
+
+
+def load_config(path: str) -> AppConfig:
+    with open(path, "r") as f:
+        _config = json.load(f)
+        config = AppConfig.model_validate_json(_config)
+        return config
+
+@asynccontextmanager
+async def app_lifespan(server: FastMCP) -> AsyncIterator[AppContext]:
+    config_path = sys.argv[1]
+    config = load_config(config_path)
+    with AppContext(config) as context:
+        yield context
+
+
+app = FastMCP(lifespan=app_lifespan)
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
